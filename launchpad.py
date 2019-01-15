7938e9710707ce3658697ca3bf0a463ac4bae45e
Eric Wuerschmidt Home <etw4nu@virginia.edu>
2019-01-14 23:26:42 -0500

    More comments

--- launchpad.py
+++ launchpad.py
@@ -6,12 +6,14 @@ output = mido.open_output('Launchpad S 1')
 #F
 notes = [0,1,2,3,4,5,16,32,48,49,50,51,64,80,96,112]
 
+#Pads on
 for i in notes:
 	output.send(mido.Message('note_on', note=i, velocity=15))
 
 
 time.sleep(5)
 
+#Pads off
 for i in notes:
 	output.send(mido.Message('note_off', note=i))
 
