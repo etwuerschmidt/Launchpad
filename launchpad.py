68f1eb87e5e913ae4549d76ed6605777a02d365e
Eric Wuerschmidt Home <etw4nu@virginia.edu>
2019-01-14 23:18:24 -0500

    Initial commit. README and basic mido

---
+++ launchpad.py
@@ -0,0 +1,17 @@
+import mido
+import time
+
+output = mido.open_output('Launchpad S 1')
+notes = [0,1,2,3,4,5,16,32,48,49,50,51,64,80,96,112]
+
+for i in notes:
+	output.send(mido.Message('note_on', note=i, velocity=15))
+
+
+time.sleep(5)
+
+for i in notes:
+	output.send(mido.Message('note_off', note=i))
+
+output.close()
+exit()
