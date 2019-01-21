192dfc67d380c008e5a52c3a21f8e1c44ab2847c
Eric Wuerschmidt Home <etw4nu@virginia.edu>
2019-01-21 01:16:15 -0500

    Added numbers and single characters to char dictionary. Created symbols dictionary and method to display symbols.

--- py_launch/py_launch/launchpad.py
+++ py_launch/py_launch/launchpad.py
@@ -213,7 +213,174 @@ class PyLaunch():
         67,
         82,
         98,
-        114, 115, 116, 117, 118]    
+        114, 115, 116, 117, 118],
+        '1': [4,
+        19, 20,
+        34, 36,
+        52,
+        68,
+        84,
+        100,
+        114, 115, 116, 117, 118],
+        '2': [3, 4, 5,
+        18, 22,
+        38,
+        53,
+        68,
+        83,
+        98,
+        114, 115, 116, 117, 118],
+        '3': [2, 3, 4, 5, 6, 
+        21,
+        36,
+        53,
+        70,
+        86,
+        98, 102,
+        115, 116, 117],
+        '4': [5,
+        20, 21,
+        35, 37,
+        50, 53,
+        66, 67, 68, 69, 70,
+        85, 
+        101,
+        117],
+        '5': [2, 3, 4, 5, 6,
+        18,
+        34, 
+        50, 51, 52, 53,
+        70,
+        86,
+        98, 102,
+        115, 116, 117],
+        '6': [4, 5, 
+        19,
+        34,
+        50, 51, 52, 53,
+        66, 70,
+        82, 86, 
+        98, 102, 
+        115, 116, 117],
+        '7': [2, 3, 4, 5, 6,
+        22, 
+        37, 
+        52, 
+        67,
+        83,
+        99,
+        115],
+        '8': [3, 4, 5, 
+        18, 22,
+        34, 38, 
+        51, 52, 53,
+        66, 70,
+        82, 86,
+        98, 102,
+        115, 116, 117],
+        '9': [3, 4, 5,
+        18, 22,
+        34, 38, 
+        50, 54,
+        67, 68, 69, 70,
+        86,
+        101,
+        115, 116],
+        '0': [3, 4, 5,
+        18, 22,
+        34, 37, 38,
+        50, 52, 54,
+        66, 68, 70,
+        82, 83, 86,
+        98, 102,
+        115, 116, 117],
+        '$': [4,
+        19, 20, 21,
+        34, 36, 38,
+        51, 52,
+        68, 69,
+        82, 84, 86,
+        99, 100, 101,
+        116],
+        '!': [4,
+        20,
+        36,
+        52,
+        68,
+        84,
+        116],
+        '?': [3, 4, 5,
+        18, 22,
+        38,
+        53, 
+        68,
+        84,
+        116]
+        }
+
+        self.symbol_mapping = {'SMILE': [2, 5,
+        18, 21,
+        34, 37,
+        64, 71,
+        81, 86,
+        98, 99, 100, 101],
+        'FROWN': [2, 5,
+        18, 21,
+        34, 37,
+        66, 67, 68, 69,
+        81, 86,
+        96, 103],
+        'HEART': [2, 3, 5, 6,
+        17, 18, 19, 20, 21, 22, 23,
+        33, 34, 35, 36, 37, 38, 39,
+        49, 50, 51, 52, 53, 54, 55,
+        66, 67, 68, 69, 70,
+        83, 84, 85,
+        100],
+        'OK': [0, 1, 2, 4, 7,
+        16, 18, 20, 22,
+        32, 34, 36, 37,
+        48, 50, 52,
+        64, 66, 68,
+        80, 82, 84, 85,
+        96, 98, 100, 102,
+        112, 113, 114, 116, 119],
+        'UP': [4,
+        19, 20, 21,
+        34, 35, 36, 37, 38,
+        49, 50, 51, 52, 53, 54, 55,
+        67, 68, 69, 
+        83, 84, 85, 
+        99, 100, 101,
+        115, 116, 117],
+        'DOWN': [3, 4, 5,
+        19, 20, 21,
+        35, 36, 37,
+        51, 52, 53,
+        65, 66, 67, 68, 69, 70, 71,
+        82, 83, 84, 85, 86,
+        99, 100, 101,
+        116],
+        'CHECK': [23,
+        38,
+        53,
+        64, 68,
+        81, 83,
+        98],
+        'NO': [3, 4, 5,
+        18, 22,
+        33, 35, 39,
+        49, 52, 55,
+        65, 69, 71,
+        82, 86,
+        99, 100, 101],
+        'PEACE': [3, 4, 5,
+        18, 20, 22,
+        33, 36, 39,
+        49, 52, 55,
+        65, 67, 68, 69, 71,
+        82, 84, 86,
+        99, 100, 101]
         }
 
         self.alignment = {'LEFT': -2,
@@ -228,15 +395,17 @@ class PyLaunch():
 
         self.lit_pads = []
 
+        self.symbol_delay = 1
         self.letter_delay = 0.5
         self.word_delay = self.letter_delay / 2
 
         self.midi_output = mido.open_output('Launchpad S 1')
+        self.midi_output.send(mido.Message('note_on', note=120, velocity=28))
 
-    def display(self, word, color=None):
+    def display_chars(self, words, color=None):
         if color != 'RANDOM':
             self.set_pad_color(color)
-        for letter in word.upper():
+        for letter in words.upper():
             if color == 'RANDOM':
                 self.set_pad_color(color)
             if letter == ' ':
@@ -249,8 +418,21 @@ class PyLaunch():
                 self.clear_lit_pads()
                 time.sleep(self.letter_delay)
 
+    def display_symbols(self, symbols, color=None):
+        if color != 'RANDOM':
+            self.set_pad_color(color)
+        for symbol in symbols.upper().split():
+            if color == 'RANDOM':
+                self.set_pad_color(color)
+            for pad in self.symbol_mapping[symbol]:
+                self.midi_output.send(mido.Message('note_on', note=pad, velocity=self.current_color))
+                self.lit_pads.append(pad)
+            time.sleep(self.symbol_delay)
+            self.clear_lit_pads()
+            time.sleep(self.symbol_delay)
+
     def clear_all_pads(self):
-        for i in range(0, 120):
+        for i in range(0, 121):
             self.midi_output.send(mido.Message('note_off', note=i))
 
     def clear_lit_pads(self):
@@ -271,16 +453,18 @@ class PyLaunch():
 
     def show_all_chars(self):
         for char in self.char_mapping.keys():
-            self.display(char)
+            self.display_chars(char)
 
     def close_launchpad(self):
+        self.clear_all_pads()
         self.midi_output.close()
 
 
 if __name__ == "__main__":
     MY_LAUNCH = PyLaunch()
-    MY_LAUNCH.display('GO HOOS', color='RANDOM')    
-    MY_LAUNCH.set_letter_delay(0.1)
-    MY_LAUNCH.show_all_chars()
+    #MY_LAUNCH.display_chars('?!$', color='RANDOM')  
+    MY_LAUNCH.display_symbols('no smile', color='RANDOM')  
+    #MY_LAUNCH.set_letter_delay(1)
+    #MY_LAUNCH.show_all_chars()
     MY_LAUNCH.close_launchpad()
     exit()
