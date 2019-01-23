80b80c9b48db49ba861cf75ee17723721f65b464
Eric Wuerschmidt <ericwuerschmidt@gmail.com>
2019-01-23 00:00:51 -0500

    Deep copy of centered char mapping. Reworked align and min dist to edge functions. Added char edge calculation method. Added error handling for invalid pads to gracefully close the existing midi connection

--- py_launch/py_launch/launchpad.py
+++ py_launch/py_launch/launchpad.py
@@ -1,3 +1,4 @@
+import copy
 import random
 import time
 import mido
@@ -318,317 +319,7 @@ class PyLaunch():
         116]
         }
 
-        self.centered_chars = {'A': [3, 4, 5, 
-        18, 22, 
-        34, 38, 
-        50, 54,
-        66, 67, 68, 69, 70,
-        82, 86,
-        98, 102,
-        114, 118],
-        'B': [2, 3, 4, 5,
-        18, 22,
-        34, 38,
-        50, 51, 52, 53,
-        66, 70,
-        82, 86,
-        98, 102,
-        114, 115, 116, 117],
-        'C': [3, 4, 5,
-        18, 22,
-        34,
-        50,
-        66,
-        82,
-        98, 102,
-        115, 116, 117],
-        'D': [2, 3, 4, 5,
-        18, 22,
-        34, 38,
-        50, 54,
-        66, 70,
-        82, 86,
-        98, 102,
-        114, 115, 116, 117],
-        'E': [2, 3, 4, 5, 6,
-        18,
-        34,
-        50, 51, 52, 53,
-        66,
-        82,
-        98,
-        114, 115, 116, 117, 118],
-        'F': [2, 3, 4, 5, 6,
-        18,
-        34,
-        50, 51, 52, 53,
-        66, 
-        82,
-        98,
-        114],
-        'G': [3, 4, 5,
-        18, 22,
-        34, 
-        50, 
-        66,
-        82, 85, 86,
-        98, 102,
-        115, 116, 117],
-        'H': [2, 6,
-        18, 22,
-        34, 38,
-        50, 51, 52, 53, 54,
-        66, 70,
-        82, 86,
-        98, 102,
-        114, 118],
-        'I': [2, 3, 4, 5, 6,
-        20,
-        36,
-        52,
-        68,
-        84,
-        100,
-        114, 115, 116, 117, 118],
-        'J': [4, 5, 6,
-        21,
-        37,
-        53,
-        69,
-        85,
-        98, 101,
-        115, 116],
-        'K': [2, 6,
-        18, 21,
-        34, 36,
-        50, 51,
-        66, 67,
-        82, 84,
-        98, 101,
-        114, 118],
-        'L': [2,
-        18,
-        34,
-        50,
-        66,
-        82,
-        98,
-        114, 115, 116, 117, 118],
-        'M': [2, 6,
-        18, 19, 21, 22,
-        34, 36, 38,
-        50, 52, 54,
-        66, 70,
-        82, 86,
-        98, 102, 
-        114, 118],
-        'N': [2, 6,
-        18, 22,
-        34, 35, 38,
-        50, 52, 54,
-        66, 69, 70,
-        82, 86,
-        98, 102,
-        114, 118],
-        'O': [3, 4, 5,
-        18, 22,
-        34, 38,
-        50, 54,
-        66, 70,
-        82, 86,
-        98, 102,
-        115, 116, 117],
-        'P': [2, 3, 4, 5,
-        18, 22,
-        34, 38,
-        50, 51, 52, 53,
-        66,
-        82,
-        98,
-        114],
-        'Q': [3, 4, 5,
-        18, 22,
-        34, 38,
-        50, 54,
-        66, 70,
-        82, 84, 86,
-        98, 101,
-        115, 116, 118],
-        'R': [2, 3, 4, 5,
-        18, 22,
-        34, 38,
-        50, 51, 52, 53,
-        66, 67,
-        82, 84,
-        98, 101,
-        114, 118],
-        'S': [3, 4, 5, 6,
-        18,
-        34,
-        51, 52, 53,
-        70,
-        86,
-        102,
-        114, 115, 116, 117],
-        'T': [2, 3, 4, 5, 6,
-        20,
-        36,
-        52,
-        68,
-        84,
-        100,
-        116],
-        'U': [2, 6,
-        18, 22,
-        34, 38,
-        50, 54,
-        66, 70,
-        82, 86,
-        98, 102,
-        115, 116, 117],
-        'V': [2, 6,
-        18, 22,
-        34, 38,
-        50, 54,
-        67, 69,
-        83, 85,
-        99, 101,
-        116],
-        'W': [2, 6,
-        18, 22,
-        34, 38,
-        50, 54,
-        66, 68, 70,
-        82, 84, 86,
-        98, 100, 102,
-        115, 117],
-        'X': [2, 6,
-        18, 22,
-        35, 37,
-        52,
-        67, 69,
-        82, 86,
-        98, 102,
-        114, 118],
-        'Y': [2, 6,
-        18, 22,
-        35, 37,
-        52,
-        68, 
-        84,
-        100,
-        116],
-        'Z': [2, 3, 4, 5, 6,
-        22,
-        37,
-        52,
-        67,
-        82,
-        98,
-        114, 115, 116, 117, 118],
-        '1': [4,
-        19, 20,
-        34, 36,
-        52,
-        68,
-        84,
-        100,
-        114, 115, 116, 117, 118],
-        '2': [3, 4, 5,
-        18, 22,
-        38,
-        53,
-        68,
-        83,
-        98,
-        114, 115, 116, 117, 118],
-        '3': [2, 3, 4, 5, 6, 
-        21,
-        36,
-        53,
-        70,
-        86,
-        98, 102,
-        115, 116, 117],
-        '4': [5,
-        20, 21,
-        35, 37,
-        50, 53,
-        66, 67, 68, 69, 70,
-        85, 
-        101,
-        117],
-        '5': [2, 3, 4, 5, 6,
-        18,
-        34, 
-        50, 51, 52, 53,
-        70,
-        86,
-        98, 102,
-        115, 116, 117],
-        '6': [4, 5, 
-        19,
-        34,
-        50, 51, 52, 53,
-        66, 70,
-        82, 86, 
-        98, 102, 
-        115, 116, 117],
-        '7': [2, 3, 4, 5, 6,
-        22, 
-        37, 
-        52, 
-        67,
-        83,
-        99,
-        115],
-        '8': [3, 4, 5, 
-        18, 22,
-        34, 38, 
-        51, 52, 53,
-        66, 70,
-        82, 86,
-        98, 102,
-        115, 116, 117],
-        '9': [3, 4, 5,
-        18, 22,
-        34, 38, 
-        50, 54,
-        67, 68, 69, 70,
-        86,
-        101,
-        115, 116],
-        '0': [3, 4, 5,
-        18, 22,
-        34, 37, 38,
-        50, 52, 54,
-        66, 68, 70,
-        82, 83, 86,
-        98, 102,
-        115, 116, 117],
-        '$': [4,
-        19, 20, 21,
-        34, 36, 38,
-        51, 52,
-        68, 69,
-        82, 84, 86,
-        99, 100, 101,
-        116],
-        '!': [4,
-        20,
-        36,
-        52,
-        68,
-        84,
-        116],
-        '?': [3, 4, 5,
-        18, 22,
-        38,
-        53, 
-        68,
-        84,
-        116]
-        }
+        self.centered_chars = copy.deepcopy(self.char_mapping)
 
         self.symbol_mapping = {'SMILE': [2, 5,
         18, 21,
@@ -697,6 +388,7 @@ class PyLaunch():
 
         self.rows = [[pad + 16*x for pad in range(8)] for x in range(0, 8)]
         self.columns = [[x + 16*pad for pad in range(8)] for x in range(0, 8)]
+        self.current_alignment = 4
 
         self.color_map = {'RED': 15,
         'GREEN': 60,
@@ -723,6 +415,12 @@ class PyLaunch():
                 time.sleep(self.word_delay)
             else:
                 for pad in self.char_mapping[character]:
+                    if pad not in range(128):
+                        try:
+                            raise ValueError('Note must be between 0 and 127, current value is %d' % pad)
+                        except:
+                            MY_LAUNCH.close_launchpad()
+                            raise
                     self.midi_output.send(mido.Message('note_on', note=pad, velocity=self.current_color))
                     self.lit_pads.append(pad)
                 time.sleep(self.letter_delay)
@@ -742,24 +440,27 @@ class PyLaunch():
             self.clear_lit_pads()
             time.sleep(self.symbol_delay)
 
+    #Aligns left edge to specified column, as long as full character can fit within pad
     def align(self, column_alignment):
         for character in self.char_mapping.keys():
             shift_value = self.calculate_min_edge_distance(self.char_mapping[character], column_alignment)
-            print("%s: %d" % (character, shift_value))
-            for ix, pad in enumerate(self.char_mapping[character]):
-                self.char_mapping[character][ix] = pad + shift_value if column_alignment >= 4 else pad - shift_value
+            print("Char %s, shift %d" % (character, shift_value))
+            for index, pad in enumerate(self.char_mapping[character]):
+                self.char_mapping[character][index] = pad + shift_value if column_alignment >= self.current_alignment else pad - shift_value
+        self.current_alignment = column_alignment
 
     def align_to_center(self):
         self.char_mapping = self.centered_chars
+        self.current_alignment = 4
 
     def calculate_min_edge_distance(self, pads, column):
-        min_distance = 1000
-        comp_edge = self.columns[column]
-        for pad in pads:
-            for edge_point in comp_edge:
-                if abs(pad - edge_point) < min_distance:
-                    min_distance = abs(pad - edge_point)
-        return min_distance
+        h_edges = self.get_horizontal_edges(pads)
+        print(h_edges)
+        print("%s, %s" % (column, self.current_alignment))
+        if column > self.current_alignment and h_edges[1] + abs(h_edges[0] - column) > 7:
+            return 7 - h_edges[1] 
+        return abs(h_edges[0] - column)
+        #return abs(h_edges[0] - column) if abs(h_edges[0] - column) <= abs(h_edges[1] - column) else abs(h_edges[1] - column)
 
     def clear_all_pads(self):
         for i in range(0, 121):
@@ -769,6 +470,16 @@ class PyLaunch():
         for i in self.lit_pads:
             self.midi_output.send(mido.Message('note_off', note=i))
             self.lit_pads = []
+
+    def get_horizontal_edges(self, pads):
+        right_edge = 0
+        left_edge = 7
+        for pad in pads:
+            for index, column in enumerate(self.columns):
+                if pad in column: 
+                    right_edge = index if index > right_edge else right_edge
+                    left_edge = index if index < left_edge else left_edge
+        return [left_edge, right_edge]
 
     def set_letter_delay(self, new_delay):
         self.letter_delay = new_delay
@@ -792,14 +503,23 @@ class PyLaunch():
 
 if __name__ == "__main__":
     MY_LAUNCH = PyLaunch()
+    #MY_LAUNCH.align(0)
+    #print(MY_LAUNCH.get_horizontal_edges(MY_LAUNCH.char_mapping['A']))
     
-    for column in range(8):
-        print(column)
-        MY_LAUNCH.align(column)
-        MY_LAUNCH.display_chars('!')
-        MY_LAUNCH.align_to_center()
-
+    
+    MY_LAUNCH.set_letter_delay(0.001)
+    for char in 'SMASH THAT F':
+        for col in [4, 3, 2, 1, 0]:
+            MY_LAUNCH.align(col)
+            MY_LAUNCH.display_chars(char)
+    
     '''
+    for col in [4, 3, 2, 1, 0]:
+        MY_LAUNCH.align(col)
+        MY_LAUNCH.display_chars('A')
+    
+    
+    MY_LAUNCH.set_letter_delay(0.2)
     MY_LAUNCH.display_chars('CENTER', color='RANDOM')  
     MY_LAUNCH.align(7)
     MY_LAUNCH.display_chars('RIGHT', color='RANDOM') 
@@ -808,8 +528,12 @@ if __name__ == "__main__":
     MY_LAUNCH.align_to_center()
     MY_LAUNCH.display_chars('CENTER', color='RANDOM')   
     #MY_LAUNCH.display_symbols('no smile', color='RANDOM')  
-    MY_LAUNCH.set_letter_delay(1)
+    
+    MY_LAUNCH.align(0)
+    MY_LAUNCH.display_chars('A')
+    MY_LAUNCH.align(3)
+    MY_LAUNCH.display_chars('A')
+    #MY_LAUNCH.show_all_chars()
     '''
-    MY_LAUNCH.show_all_chars()
     MY_LAUNCH.close_launchpad()
     exit()
