f1b4c73dfe2895f16ed079c9a12c4a40540efa15
Eric - Desktop <etwuerschmidt@yahoo.com>
2019-10-27 18:43:25 -0400

    Updated import in display.py

--- display.py
+++ display.py
@@ -1,8 +1,6 @@
-from py_launch import *
+import py_launch
 
-MY_LAUNCH = PyLaunch()
-#MY_LAUNCH.display_chars("Hello world")
-
+MY_LAUNCH = py_launch.PyLaunch()
 MY_LAUNCH.scroll("hello", direction='X', positive=False)
 MY_LAUNCH.scroll("world", direction='Y')
 MY_LAUNCH.close_launchpad()
